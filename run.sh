1126fa485bea90b711a7f0eda2c590cbd3cc484b
Cesar Gonzalez Fernandez <cesar.gon.fer@gmail.com>
2017-12-06 17:38:40 +0100

    Update run.sh with all valid params

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # while true; do python twitterstream.py $MAX_TWEETS_PER_BATCH > /output/$(date '+%Y%m%d_%H%M'); sleep $(($SLEEP_TIME * 60)); done
-while true; do python twitterstream.py --max_tweets $MAX_TWEETS_PER_BATCH --location=$LOCATION_FILTER > /output/$(date '+%Y%m%d_%H%M'); done 
+while true; do python twitterstream.py --max_tweets $MAX_TWEETS_PER_BATCH --location=$LOCATION_FILTER > /output/$(date '+%Y%m%d_%H%M'); sleep $(($SLEEP_TIME * 60)); done 
